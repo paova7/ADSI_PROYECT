0611c5ac0220b8f20134019e8095311dc650796d
Paula Ovalle <paova.2118@gmail.com>
2022-11-05 00:35:47 -0500

    Primer push
    Esta listo el frontend del proyecto, se está iniciando con la conexión de php, hasta se realiza la consulta de los datos del estudiante, se estan solucionando algunos detalles con los avisos que deben aparecer cuando el numero de documento no existe

---
+++ paginas/consulta.php
@@ -0,0 +1,180 @@
+<!DOCTYPE html>
+<html lang="es" class="h-100 ">
+
+<head>
+    <meta charset="UTF-8">
+
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+
+    <title>Inicio</title>
+
+    <!-- Esto es bootstrap -->
+    <link rel="stylesheet" href="../plugins/bootstrap/css/bootstrap.min.css">
+    <script src="../plugins/bootstrap/js/bootstrap.bundle.js"></script>
+
+    <!-- Este es mi estilo -->
+    <link rel="stylesheet" href="../paginas/styles/mainstyles.css">
+
+    <!-- Fondo del LOGIN -->
+    <style>
+        body {
+            background-image: url('../media/imagenes/fondoprincipal.jpg');
+            background-repeat: no-repeat;
+            background-attachment: fixed;
+            background-size: cover;
+            background-position: center center;
+        }
+    </style>
+
+</head>
+
+<body background="../media/imagenes/fondoprincipal.jpg" ;>
+
+    <!-- MENÚ -->
+    <header>
+
+        <!-- Navbar desplegable al dar click y contenido-->
+        <div id="navbarHeader" class="collapse bg-dark ">
+            <div class="container">
+                <div class="row">
+                    <div class="col-sm-8 col-md-7 py-4">
+                        <h4 class="text-white">Acercar de nosotros</h4>
+                        <p class="text-muted">Somos una institución que facilita soluciones educativas tanto para personas como para colegios y otras instituciones; nos enfocamos en facilitar procesos educativos y brindar cursos de preparación especializados.</p>
+                    </div>
+                    <div class="col-sm-4 offset-md-1 py-4">
+                        <h4 class="text-white">Contactanos</h4>
+                        <ul class="list-unstyled">
+                            <li><a href="#" class="text-white">WhatsApp</a></li>
+                            <li><a href="#" class="text-white">Vísitanos en Facebook</a></li>
+                            <li><a href="#" class="text-white">Correo</a></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <!-- Navbar botones de los modulos de inicio, estidiante e ingreso a personal -->
+        <div class="navbar fixed-top navbar-dark bg-dark shadow-sm">
+            <div class="container">
+                <!-- logo en el navbar -->
+                <a href="../index.html" class="navbar-brand d-flex align-items-center">
+                    <img src="../media/logo/logoinstitutoclei.png" class="me-2" wsidth="40" height="40">
+                    <strong>Instituto CLEI</strong>
+                </a>
+                <nav class="fw-bold nav nav-masthead nav-pills nav-fill justify-content-center float-md-end">
+                    <ul class="nav nav-pills">
+                        <!-- Boton de inicio -->
+                        <a class="nav-link" style="margin-left: 20px" href="../index.html">Inicio</a>
+                        <!-- Boton de Estudiante -->
+                        <a class="nav-link active" aria-current="page" style="margin-left: 20px" href="../paginas/consultaestudiante.html">Estudiante</a>
+                        <!-- desplegable de ingreso "Empleado" o "Administrador" -->
+                        <li class="nav-item dropdown">
+                            <a class="nav-link dropdown-toggle" data-bs-toggle="dropdown" style="margin-left: 10px" href="#" role="button" aria-expanded="false">Ingreso de personal</a>
+                            <ul class="dropdown-menu">
+                                <li><a class="dropdown-item" href="../paginas/empleado.html">Empleado</a></li>
+                                <li><a class="dropdown-item" href="../paginas/administrador.html">Administrador</a></li>
+                            </ul>
+                        </li>
+                    </ul>
+                </nav>
+                <!-- Boton del navbar -->
+                <button class="navbar-toggler " type="button" data-bs-toggle="collapse" data-bs-target="#navbarHeader" aria-controls="navbarHeader" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+            </div>
+        </div>
+    </header>
+
+    <!-- CONTENIDO DEL MÓDULO DE CONSULTA ESTUDIANTES-->
+
+<
+
+    <main style="padding-top: 120px;">
+<?php
+
+
+$consultaestudiante = $_GET["documento"];
+
+require_once '../conexion/conexion.php';  /*LLama la conexión*/
+$db = new db_conexion();               /*Abre la base de datos*/
+
+$sql = "SELECT * FROM estudiantes
+      WHERE documento_estudiante ='$consultaestudiante'";
+
+        $resultado = mysqli_query($db->conectar(), $sql);         /*pasa la query a la variable resultado*/
+        while ($registro = mysqli_fetch_array($resultado)) {      /*pasa a vector*/
+
+            $db->db_cerrar();
+?>
+        </div>
+        <br>
+            <div class="container p-4 p-md-12 border rounded-4 border bg-light bg-gradient p-2 border-5 text-center" style="padding: 100px;">
+                <p class="fs-2 fw-bolder">Bienveniedo <?php echo $registro['nombre_estudiante']; ?></p>
+                <p>Podrás visiualizar tu estado en la institución </p>
+                <br>
+                <div class="bd-example-snippet bd-code-snippet text-start">
+                    <p class="fs-8 fw-bolder" ;>Datos personales</p>
+                    <div class="bd-example">
+                        <table class="table table-sm table-bordered">
+                            <tbody>
+                                <tr>
+                                    <td scope="col">Nombres :<?php echo " " . $registro['nombre_estudiante']; ?></td>
+                                    <td scope="col">Apellidos: <?php echo  " " . $registro['apellido_estudiante']; ?></td>
+
+                                </tr>
+                                <tr>
+                                    <td>Número de documento:<?php echo  " " . $registro['documento_estudiante']; ?></td>
+                                    <td>Fecha de nacimiento:<?php echo " " . $registro['fechadenacimiesto_estudiante']; ?></td>
+                                </tr>
+                                <tr>
+                                    <td>NUMERO DE TELEFONO:<?php echo " " . $registro['numerocontacto_estudiante']; ?></td>
+                                    <td>ciudad de residencia:<?php echo " " . $registro['ciudad_estudiante']; ?> </td>
+                                </tr>
+                                <tr>
+                                    <td scope="col">Email:<?php echo " " . $registro['email_estudiante']; ?></td>
+                                    <td scope="col">Numero del acudiente: <?php echo  " " . $registro['acudiente__estudiante']; ?></td>
+
+                                </tr>
+                            </tbody>
+                        </table>
+                    </div>
+                    <p class="fs-8 fw-bolder" ;>Datos del curso</p>
+                    <div class="bd-example">
+                        <table class="table table-sm table-bordered">
+                            <tbody>
+                                <tr>
+                                    <td scope="col"> Curso:<?php echo " " . $registro['curso_estudiante']; ?></td>
+                                    <td scope="col">Horario de clases: <?php echo  " " . $registro['horario_estudiante']; ?></td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </div>
+                    <p class="fs-8 fw-bolder" ;>Pagos realizados</p>
+                    <div class="bd-example">
+                        <table class="table table-sm table-bordered">
+                            <tbody>
+                                <tr>
+                                    <td scope="col"> Fecha del pago:<?php echo " " . $registro['curso_estudiante']; ?></td>
+                                    <td scope="col">Horario de clases: <?php echo  " " . $registro['horario_estudiante']; ?></td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </div>
+                    <div class="dropdown">
+                        <a href="../paginas/consultaestudiante.php"><button type="button" class="btn btn-danger" style="background-color: rgb(168, 4, 4);">Realizar otra consulta</button></a>
+
+                    </div>
+                </div>
+
+                <small class="text-muted"> Año 2022</small>
+            </div>
+
+ <?php
+        }
+    
+ ?>           
+</body>
+
+</html>
---
+++ paginas/consultaestudiante.php
@@ -0,0 +1,204 @@
+<!DOCTYPE html>
+<html lang="es" class="h-100 ">
+
+<head>
+  <meta charset="UTF-8">
+
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+
+  <title>Inicio</title>
+
+  <!-- Esto es bootstrap -->
+  <link rel="stylesheet" href="../plugins/bootstrap/css/bootstrap.min.css">
+  <script src="../plugins/bootstrap/js/bootstrap.bundle.js"></script>
+
+  <!-- Este es mi estilo -->
+  <link rel="stylesheet" href="../paginas/styles/mainstyles.css">
+
+  <!-- Fondo del LOGIN -->
+  <style>
+    body {
+      background-image: url('../media/imagenes/fondoprincipal.jpg');
+      background-repeat: no-repeat;
+      background-attachment: fixed;
+      background-size: cover;
+      background-position: center center;
+
+
+    }
+  </style>
+  <script>
+    function enviame(laUrl, elId) {
+      var elform = document.getElementById("MiForm");
+      var eldato = document.getElementById("documento");
+      eldato.value = elId;
+      elform.action = laUrl;
+      elform.submit();
+    }
+  </script>
+
+</head>
+
+<body background="../media/imagenes/fondoprincipal.jpg" ;>
+
+  <!-- MENÚ -->
+  <header>
+
+    <!-- Navbar desplegable al dar click y contenido-->
+    <div id="navbarHeader" class="collapse bg-dark ">
+      <div class="container">
+        <div class="row">
+          <div class="col-sm-8 col-md-7 py-4">
+            <h4 class="text-white">Acercar de nosotros</h4>
+            <p class="text-muted">Somos una institución que facilita soluciones educativas tanto para personas como para colegios y otras instituciones; nos enfocamos en facilitar procesos educativos y brindar cursos de preparación especializados.</p>
+          </div>
+          <div class="col-sm-4 offset-md-1 py-4">
+            <h4 class="text-white">Contactanos</h4>
+            <ul class="list-unstyled">
+              <li><a href="#" class="text-white">WhatsApp</a></li>
+              <li><a href="#" class="text-white">Vísitanos en Facebook</a></li>
+              <li><a href="#" class="text-white">Correo</a></li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <!-- Navbar botones de los modulos de inicio, estidiante e ingreso a personal -->
+    <div class="navbar fixed-top navbar-dark bg-dark shadow-sm">
+      <div class="container">
+        <!-- logo en el navbar -->
+        <a href="../index.html" class="navbar-brand d-flex align-items-center">
+          <img src="../media/logo/logoinstitutoclei.png" class="me-2" wsidth="40" height="40">
+          <strong>Instituto CLEI</strong>
+        </a>
+        <nav class="fw-bold nav nav-masthead nav-pills nav-fill justify-content-center float-md-end">
+          <ul class="nav nav-pills">
+            <!-- Boton de inicio -->
+            <a class="nav-link" style="margin-left: 20px" href="../index.html">Inicio</a>
+            <!-- Boton de Estudiante -->
+            <a class="nav-link active" aria-current="page" style="margin-left: 20px" href="../paginas/consultaestudiante.html">Estudiante</a>
+            <!-- desplegable de ingreso "Empleado" o "Administrador" -->
+            <li class="nav-item dropdown">
+              <a class="nav-link dropdown-toggle" data-bs-toggle="dropdown" style="margin-left: 10px" href="#" role="button" aria-expanded="false">Ingreso de personal</a>
+              <ul class="dropdown-menu">
+                <li><a class="dropdown-item" href="../paginas/empleado.html">Empleado</a></li>
+                <li><a class="dropdown-item" href="../paginas/administrador.html">Administrador</a></li>
+              </ul>
+            </li>
+          </ul>
+        </nav>
+        <!-- Boton del navbar -->
+        <button class="navbar-toggler " type="button" data-bs-toggle="collapse" data-bs-target="#navbarHeader" aria-controls="navbarHeader" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+      </div>
+    </div>
+  </header>
+
+  <!-- CONTENIDO DEL MÓDULO DE CONSULTA ESTUDIANTES-->
+
+  <main style="padding-top: 120px;">
+
+
+
+    <div class="container-fluid col-xl-10 col-xxl-8 px-4 py-5 text-white">
+      <div class=" row align-items-center  py-5">
+        <!-- Parte 1 del contenido  -->
+        <div class="col-lg-7 text-center text-lg-start">
+          <h1 class="display-4 fw-bold lh-1 mb-3">Consulta tu estado en el Instituto CLEI</h1>
+          <p class="col-lg-10 fs-4">Podrás visiaulizar tu información personal y los pagos que has realizado hasta la fecha. </p>
+        </div>
+        <!-- Parte 2 formulario de ingreso para realizar la consulta -->
+        <div class="col-md-10 mx-auto col-lg-5" style="color:#000000">
+          <form class="p-4 p-md-12 border rounded-4 border bg-light bg-gradient p-2 border-5 text-center" style="padding: 100px;" method="POST" action="consultaestudiante.php" name="MiForm">
+            <p class="fs-3 fw-bolder">Escribe tu documento</p>
+            <p>Ingresa el número de documento del estudiante sin espacios ni puntos. </p>
+            <br>
+            <div class="form-floating mb-3 form-group" label for="documento">
+              <input type="text" name="documento" class="form-control" id="floatingPassword" placeholder="text">
+              <label for="floatingPassword">Número de documento</label>
+            </div>
+
+            <input class="w-100 btn btn-lg btn-primary" type="submit" value="Consultar" class="btn btn-primary" name="btn_consultar">
+
+            <?php
+
+            require_once '../conexion/conexion.php';  /*LLama la conexión*/
+            $db = new db_conexion();               /*Abre la base de datos*/
+
+            $consultaestudiante = "";
+
+            if (isset($_POST['btn_consultar'])) {
+
+              $consultaestudiante = $_POST["documento"];   /*Pide la cedula por POST*/
+              if ($consultaestudiante == "") {             /*si la cedula esta en blanco informa mensaje, sino hace la consulta*/
+              // Imprime una alerta cuando el campo está vacio
+                echo '
+          <br>
+          <br>        
+          <div class="container formulario"></div>
+          <center>
+            <div class="alert alert-danger" role="alert">
+            <strong>Error!</strong> El numero de Cedula es Obligatorio.
+            </div>
+          </center>
+          </div>';
+              } elseif ($consultaestudiante <> 0) { //Realiza la consuta 
+
+                $sql = "SELECT documento_estudiante FROM estudiantes
+      WHERE documento_estudiante ='$consultaestudiante'";
+
+                $resultado = mysqli_query($db->conectar(), $sql);         /*pasa la query a la variable resultado*/
+                while ($registro = mysqli_fetch_array($resultado)) {      /*pasa a vector*/
+                  $db->db_cerrar();
+
+                  if ($consultaestudiante == $registro['documento_estudiante']) {//Si eciste realiza la consulta e informa que puede ver el informe
+            ?>
+                    <br>
+                    <br>
+                    <a href="consulta.php?documento=<?php echo $consultaestudiante ?>"><button type="button" method="POST" action="consulta.php" class="btn btn-success">¡Estudiante encontrado! <br> Ver consulta</button></a>
+
+            <?php
+
+                  } 
+                  
+                  else {//Sin definir
+
+                    echo '
+          <br>
+          <br>        
+          <div class="container formulario"></div>
+          <center>
+            <div class="alert alert-danger" role="alert">
+            <strong>Error!</strong> El numero de Cedula no existe.
+            </div>
+          </center>
+          </div>';
+                  }
+                }
+              }
+            }
+            ?>
+
+            <hr class="my-4">
+            <small class="text-muted"> Año 2022</small>
+          </form>
+        </div>
+      </div>
+    </div>
+  </main>
+
+
+  </div>
+
+
+</body>
+
+
+
+
+
+</html>
